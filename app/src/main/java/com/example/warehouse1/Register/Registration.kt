d4991bed5f170f04d7ca693fc2e9c18a10aea675
Kowshik <kowshikkarmagam@gmail.com>
2022-03-11 20:46:02 +0530

    Have to implement AWS amplify

--- app/src/main/java/com/example/warehouse1/Register/Registration.kt
+++ app/src/main/java/com/example/warehouse1/Register/Registration.kt
@@ -1,4 +1,4 @@
-package com.example.warehouse1.login
+package com.example.warehouse1.Register
 
 import android.content.Intent
 import android.os.Bundle
@@ -10,37 +10,56 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.warehouse1.R
-import com.example.warehouse1.Register.Registration
-import com.example.warehouse1.SelectWarehouse
+import com.example.warehouse1.login.LoginActivity
 import com.google.android.material.textfield.TextInputLayout
 
 
-class LoginActivity : AppCompatActivity() {
+class Registration : AppCompatActivity() {
+    var name: EditText? = null
     var email: EditText? = null
+    var phone: EditText? = null
     var password: EditText? = null
-    var login: Button? = null
-    var register: TextView? = null
+    var register: Button? = null
+    var login: TextView? = null
+    var isNameValid = false
     var isEmailValid = false
+    var isPhoneValid = false
     var isPasswordValid = false
+    var nameError: TextInputLayout? = null
     var emailError: TextInputLayout? = null
+    var phoneError: TextInputLayout? = null
     var passError: TextInputLayout? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_registration)
+        name = findViewById<View>(R.id.name) as EditText
         email = findViewById<View>(R.id.email) as EditText
+        phone = findViewById<View>(R.id.phone) as EditText
         password = findViewById<View>(R.id.password) as EditText
-        login = findViewById<View>(R.id.login) as Button
-        register = findViewById<View>(R.id.register) as TextView
+        login = findViewById<View>(R.id.login) as TextView
+        register = findViewById<View>(R.id.register) as Button
+        nameError = findViewById<View>(R.id.nameError) as TextInputLayout
         emailError = findViewById<View>(R.id.emailError) as TextInputLayout
+        phoneError = findViewById<View>(R.id.phoneError) as TextInputLayout
         passError = findViewById<View>(R.id.passError) as TextInputLayout
-        login!!.setOnClickListener { SetValidation() }
-        register!!.setOnClickListener { // redirect to RegisterActivity
-            val intent = Intent(applicationContext, Registration::class.java)
+        register!!.setOnClickListener { SetValidation() }
+        login!!.setOnClickListener { // redirect to LoginActivity
+            val intent = Intent(applicationContext, LoginActivity::class.java)
             startActivity(intent)
+            finish()
         }
     }
 
-    private fun SetValidation() {
+    fun SetValidation() {
+        // Check for a valid name.
+       if (name!!.text.toString().isEmpty()) {
+            nameError!!.error = resources.getString(R.string.name_error)
+            isNameValid = false
+        } else {
+            isNameValid = true
+            nameError!!.isErrorEnabled = false
+        }
+
         // Check for a valid email address.
         if (email!!.text.toString().isEmpty()) {
             emailError!!.error = resources.getString(R.string.email_error)
@@ -53,7 +72,16 @@ class LoginActivity : AppCompatActivity() {
             emailError!!.isErrorEnabled = false
         }
 
-        // Check for a valid password.
+        // Check for a valid phone number.
+       if (phone!!.text.toString().isEmpty()) {
+            phoneError!!.error = resources.getString(R.string.phone_error)
+            isPhoneValid = false
+        } else {
+            isPhoneValid = true
+            phoneError!!.isErrorEnabled = false
+        }
+
+        //Check for a valid password.
         if (password!!.text.toString().isEmpty()) {
             passError!!.error = resources.getString(R.string.password_error)
             isPasswordValid = false
@@ -64,11 +92,9 @@ class LoginActivity : AppCompatActivity() {
             isPasswordValid = true
             passError!!.isErrorEnabled = false
         }
-        if (isEmailValid && isPasswordValid) {
-
-            Toast.makeText(applicationContext, "Successfully", Toast.LENGTH_SHORT).show()
-            val intent = Intent(applicationContext, SelectWarehouse::class.java)
-            startActivity(intent)
-        }
+       // if (isNameValid && isEmailValid && isPhoneValid && isPasswordValid) {
+            Toast.makeText(applicationContext, "Successfully Regstered", Toast.LENGTH_SHORT).show()
+            finish()
+       // }
     }
 }
---
+++ app/src/main/java/com/example/warehouse1/login/LoginActivity1.kt
@@ -0,0 +1,27 @@
+package com.example.warehouse1.login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.EditText
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.warehouse1.R
+import com.example.warehouse1.dashboard.Dashboard
+
+class LoginActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login1)
+        findViewById<Button>(R.id.login).setOnClickListener{
+            if (findViewById<EditText>(R.id.username).text.trim().toString()=="1" && findViewById<EditText>(R.id.password).text.trim().toString()=="1")
+            {
+                Toast.makeText(this,"Logging in!!",Toast.LENGTH_SHORT).show()
+                val intent = Intent(applicationContext, Dashboard::class.java)
+                startActivity(intent)
+            }else{
+                    Toast.makeText(this,"Wrong credentials !! , Try again ",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
